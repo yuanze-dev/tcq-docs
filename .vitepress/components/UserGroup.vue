2a1a99c95e1dfc6551354be71d17e8370ed32318
Guoqi Sun <sun412926@gmail.com>
2024-06-16 10:58:19 +0800

    fix: fix user qrcode

--- .vitepress/components/UserGroup.vue
+++ .vitepress/components/UserGroup.vue
@@ -1,5 +1,5 @@
 <template>
   <p>欢迎加入芦笋提词器用户交流群，你可以在群内随时反馈使用问题以及功能建议，期待你的声音～</p>
-  <ImgCenter><img src="/.gitbook/assets/quncode.png" alt="" width="200"></ImgCenter>
+  <ImgCenter><img src="/public/.gitbook/assets/quncode.png" alt="" width="200"></ImgCenter>
   <ImgDesc>芦笋提词器用户交流群</ImgDesc>
 </template>
